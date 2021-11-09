8ff4427b7d8c66e36b57af35a20047a12bbc2943
Erik Plachta <eplachta@gmail.com>
2021-11-09 15:32:22 -0500

    Update Windows_AudioLevel.ps1

--- assets/powershell/Windows_AudioLevel.ps1
+++ assets/powershell/Windows_AudioLevel.ps1
@@ -1,12 +1,12 @@
 <# 
     Author: Erik Plachta
     Date: 11/09/2021
-    Purpose: Manage Windows Audio Levels With PowerShella
+    Purpose: Manage Windows Audio Volume Levels With PowerShella
     Summary: Using PowerShell to create a COM Object of the type Windows Shell. Then running Windows Shell function function SendKeys() with the parameters `[char]173`, `[char]174`, or `[char]175`.
 #>
 
 <# % FUNCTION - START #>
-function Set-Speaker($audio_Level){
+function set_AudioLevel($audioLevel){
     <# Loop to adjust audio level based on volume
 
         Order of Operations    
@@ -18,29 +18,29 @@ function Set-Speaker($audio_Level){
         Example: ( Remember incrementing by 1% )
             
             #Sets volume to 60%
-            Set-Speaker -Volume 60
+            set_AudioLevel -Volume 60
 
             #Sets volume to 80%
-            Set-Speaker -Volume 80
+            set_AudioLevel -Volume 80
 
             #Sets volume to 100%
-            Set-Speaker -Volume 100
+            set_AudioLevel -Volume 100
     #>
 
     ## 1. Create Shell  Object
     $wshShell = new-object -com wscript.shell;
 
-     ## 2. Make each level 1 %. by Default assumes 2%, want to be 1 so can do 0 - 100% level
-     $volume_Level = $audio_Level / 2
-     
-     ## 3. Sets Volume Level to 0
+     ## 2. Sets Volume Level to 0
      1..50 | % { # 50 time loop
          ##Set Volume Level to ZERO ( 50 = MAX or 100% level )
          $wshShell.SendKeys([char]174)
      }
      
+     ## 3. Make each level 1 %. by Default assumes 2%, want to be 1 so can do 0 - 100% level
+     $audioLevel = $audioLevel / 2
+     
      ## 4. Increments UP to Level based on provided INT $audio_Level
-     1..$volume_Level | % { ## Loop from 1 to volume level provide
+     1..$audioLevel | % { ## Loop from 1 to volume level provide
          ## Adjust volume level UP for by one for each level loop
          $wshShell.SendKeys([char]175)
      }
