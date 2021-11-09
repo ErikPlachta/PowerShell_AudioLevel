3d847a7363c92adaa02e6bce1056f85b6ce10294
Erik Plachta <eplachta@gmail.com>
2021-11-09 15:23:15 -0500

    Update Windows_AudioLevel.ps1
    Spelling fix

--- assets/powershell/Windows_AudioLevel.ps1
+++ assets/powershell/Windows_AudioLevel.ps1
@@ -1,7 +1,7 @@
 <# 
     Author: Erik Plachta
     Date: 11/09/2021
-    Purpose: Manage Windows Audio Levels With PowerShell
+    Purpose: Manage Windows Audio Levels With PowerShella
     Summary: Using PowerShell to create a COM Object of the type Windows Shell. Then running Windows Shell function function SendKeys() with the parameters `[char]173`, `[char]174`, or `[char]175`.
 #>
 
@@ -11,7 +11,7 @@ function Set-Speaker($audio_Level){
 
         Order of Operations    
             1. Create Windows Shell object
-            2. Divides $audio_Level by 50% because command uses 2% by default, but we are asssuming 1%.        
+            2. Divides $audio_Level by 50% because command uses 2% by default, but we are assuming 1%.        
             3. Sets Volume Level to 0
             4. Increments UP to Level based on provided INT $audio_Level
 
@@ -27,20 +27,20 @@ function Set-Speaker($audio_Level){
             Set-Speaker -Volume 100
     #>
 
-    #1. Create Shell  Object
+    ## 1. Create Shell  Object
     $wshShell = new-object -com wscript.shell;
 
-     #2. Make each level 1 %. by Default assumes 2%, want to be 1 so can do 0 - 100% level
+     ## 2. Make each level 1 %. by Default assumes 2%, want to be 1 so can do 0 - 100% level
      $volume_Level = $audio_Level / 2
      
-     ##3. 50 time loop
-     1..50 | % {
+     ## 3. Sets Volume Level to 0
+     1..50 | % { # 50 time loop
          ##Set Volume Level to ZERO ( 50 = MAX or 100% level )
          $wshShell.SendKeys([char]174)
      }
      
-     ##4. Loop from 1 to volume level provide
-     1..$volume_Level | % {
+     ## 4. Increments UP to Level based on provided INT $audio_Level
+     1..$volume_Level | % { ## Loop from 1 to volume level provide
          ## Adjust volume level UP for by one for each level loop
          $wshShell.SendKeys([char]175)
      }
